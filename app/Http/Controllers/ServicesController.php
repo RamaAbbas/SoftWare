5f0e1f4e24442a4d7a89ebe6e4262e755942c43e
Rama Abbas <r.abbas@sahmtech.com>
2024-08-19 09:09:26 +0300

    .

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -42,12 +42,11 @@ class ServicesController extends Controller
     {
         $language = $request->header('Lang');
         $defaultLanguage = 'en';
-
         $locale = $language ? substr($language, 0, 2) : $defaultLanguage;
 
-
         $services = Service::with(['requirment', 'for_who', 'how_it_work'])->get();
-        if ($services->count() == 0) {
+
+        if ($services->isEmpty()) {
             return response()->json([
                 'success' => 0,
                 'result' => null,
@@ -58,55 +57,64 @@ class ServicesController extends Controller
         try {
             $processedServices = $services->map(function ($service) use ($locale, $defaultLanguage) {
 
-                $jsonDataB = json_decode($service->description, true);
+                if (is_string($service->description)) {
+                    $description = json_decode($service->description, true);
+                    $service->description = $description[$locale] ?? $description[$defaultLanguage] ?? __('Language not supported');
+                }
 
-                $service->description = $jsonDataB[$locale] ?? $jsonDataB[$defaultLanguage] ?? 'Language not supported';
 
                 $service->requirment = $service->requirment->map(function ($requirment) use ($locale, $defaultLanguage) {
-                    $jsonDataB = json_decode($requirment->descripton_of_requirment, true);
-
-                    $requirment->descripton_of_requirment = $jsonDataB[$locale] ?? $jsonDataB[$defaultLanguage] ?? 'Language not supported';
-
-                    return $requirment; //->makeHidden(['b', 'c']);
+                    if (is_string($requirment->descripton_of_requirment)) {
+                        $description = json_decode($requirment->descripton_of_requirment, true);
+                        $requirment->descripton_of_requirment = $description[$locale] ?? $description[$defaultLanguage] ?? __('Language not supported');
+                    }
+                    return $requirment;
                 });
+
 
                 $service->for_who = $service->for_who->map(function ($related) use ($locale, $defaultLanguage) {
-                    $jsonDataB = json_decode($related->benefit_name, true);
-                    $jsonDataC = json_decode($related->benefit_description, true);
-
-                    $related->benefit_name = $jsonDataB[$locale] ?? $jsonDataB[$defaultLanguage] ?? 'Language not supported';
-                    $related->benefit_description = $jsonDataC[$locale] ?? $jsonDataC[$defaultLanguage] ?? 'Language not supported';
-
-                    return $related; //->makeHidden(['b', 'c']);
+                    if (is_string($related->benefit_name)) {
+                        $name = json_decode($related->benefit_name, true);
+                        $related->benefit_name = $name[$locale] ?? $name[$defaultLanguage] ?? __('Language not supported');
+                    }
+                    if (is_string($related->benefit_description)) {
+                        $description = json_decode($related->benefit_description, true);
+                        $related->benefit_description = $description[$locale] ?? $description[$defaultLanguage] ?? __('Language not supported');
+                    }
+                    return $related;
                 });
+
 
                 $service->how_it_work = $service->how_it_work->map(function ($related) use ($locale, $defaultLanguage) {
-                    $jsonDataB = json_decode($related->name_of_step, true);
-                    $jsonDataC = json_decode($related->description_of_step, true);
-
-                    $related->name_of_step = $jsonDataB[$locale] ?? $jsonDataB[$defaultLanguage] ?? 'Language not supported';
-                    $related->description_of_step = $jsonDataC[$locale] ?? $jsonDataC[$defaultLanguage] ?? 'Language not supported';
-
-                    return $related; //->makeHidden(['b', 'c']);
+                    if (is_string($related->name_of_step)) {
+                        $stepName = json_decode($related->name_of_step, true);
+                        $related->name_of_step = $stepName[$locale] ?? $stepName[$defaultLanguage] ?? __('Language not supported');
+                    }
+                    if (is_string($related->description_of_step)) {
+                        $stepDescription = json_decode($related->description_of_step, true);
+                        $related->description_of_step = $stepDescription[$locale] ?? $stepDescription[$defaultLanguage] ?? __('Language not supported');
+                    }
+                    return $related;
                 });
 
-                return $service; //->makeHidden(['b', 'c']);
+                return $service;
             });
+
             return response()->json([
                 'success' => 1,
                 'result' => $processedServices,
-                'message' => __("app.data_returnd_sucssesfully")
+                'message' => __("app.data_returned_successfully")
             ], 200);
-
-            // return response()->json($processedServices);
         } catch (Exception $e) {
             return response()->json([
                 'success' => 0,
                 'result' => null,
-                'message' => $e
-            ], 200);
+                'message' => $e->getMessage()
+            ], 500);
         }
     }
+
+
     ///////////////////////////////////////////////////////////////
     public function show_all()
     {
@@ -166,8 +174,8 @@ class ServicesController extends Controller
     {
         $language = $request->header('Lang');
         $defaultLanguage = 'en';
-
         $locale = $language ? substr($language, 0, 2) : $defaultLanguage;
+
         try {
             $service = Service::with(['requirment', 'for_who', 'how_it_work'])->findOrFail($id);
 
@@ -180,54 +188,60 @@ class ServicesController extends Controller
             }
 
 
-            $jsonDataB = json_decode($service->description, true);
-
-            $service->description = $jsonDataB[$locale] ?? $jsonDataB[$defaultLanguage] ?? 'Language not supported';
+            if (is_string($service->description)) {
+                $description = json_decode($service->description, true);
+                $service->description = $description[$locale] ?? $description[$defaultLanguage] ?? __('Language not supported');
+            }
 
 
             $service->requirment = $service->requirment->map(function ($related) use ($locale, $defaultLanguage) {
-                $jsonDataB = json_decode($related->descripton_of_requirment, true);
-
-                $related->descripton_of_requirment = $jsonDataB[$locale] ?? $jsonDataB[$defaultLanguage] ?? 'Language not supported';
-
+                if (is_string($related->descripton_of_requirment)) {
+                    $description = json_decode($related->descripton_of_requirment, true);
+                    $related->descripton_of_requirment = $description[$locale] ?? $description[$defaultLanguage] ?? __('Language not supported');
+                }
                 return $related;
             });
+
 
             $service->for_who = $service->for_who->map(function ($related) use ($locale, $defaultLanguage) {
-                $jsonDataB = json_decode($related->benefit_name, true);
-                $jsonDataC = json_decode($related->benefit_description, true);
-
-                $related->translated_text_b = $jsonDataB[$locale] ?? $jsonDataB[$defaultLanguage] ?? 'Language not supported';
-                $related->translated_text_c = $jsonDataC[$locale] ?? $jsonDataC[$defaultLanguage] ?? 'Language not supported';
-
+                if (is_string($related->benefit_name)) {
+                    $name = json_decode($related->benefit_name, true);
+                    $related->translated_text_b = $name[$locale] ?? $name[$defaultLanguage] ?? __('Language not supported');
+                }
+                if (is_string($related->benefit_description)) {
+                    $description = json_decode($related->benefit_description, true);
+                    $related->translated_text_c = $description[$locale] ?? $description[$defaultLanguage] ?? __('Language not supported');
+                }
                 return $related;
             });
 
-            $service->how_it_work  = $service->how_it_work->map(function ($related) use ($locale, $defaultLanguage) {
-                $jsonDataB = json_decode($related->name_of_step, true);
-                $jsonDataC = json_decode($related->description_of_step, true);
 
-                $related->translated_text_b = $jsonDataB[$locale] ?? $jsonDataB[$defaultLanguage] ?? 'Language not supported';
-                $related->translated_text_c = $jsonDataC[$locale] ?? $jsonDataC[$defaultLanguage] ?? 'Language not supported';
-
+            $service->how_it_work = $service->how_it_work->map(function ($related) use ($locale, $defaultLanguage) {
+                if (is_string($related->name_of_step)) {
+                    $stepName = json_decode($related->name_of_step, true);
+                    $related->translated_text_b = $stepName[$locale] ?? $stepName[$defaultLanguage] ?? __('Language not supported');
+                }
+                if (is_string($related->description_of_step)) {
+                    $stepDescription = json_decode($related->description_of_step, true);
+                    $related->translated_text_c = $stepDescription[$locale] ?? $stepDescription[$defaultLanguage] ?? __('Language not supported');
+                }
                 return $related;
             });
+
             return response()->json([
                 'success' => 1,
                 'result' => $service,
-                'message' => __('app.service_returned_sucsessfully')
+                'message' => __('app.service_returned_successfully')
             ], 200);
-
-
-            //  return response()->json($service->makeHidden(['b', 'c']));
         } catch (Exception $e) {
             return response()->json([
                 'success' => 0,
                 'result' => null,
-                'message' => $e
-            ], 200);
+                'message' => $e->getMessage()
+            ], 500);
         }
     }
+
     /* public function show($id)
     {
         //  $service = Service::findOrFail($id);
