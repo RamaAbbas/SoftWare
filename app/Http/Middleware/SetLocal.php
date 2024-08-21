72275edff545a306a8e07bd21c85d779f863d238
Rama Abbas <r.abbas@sahmtech.com>
2024-08-21 12:29:11 +0300

    .

--- app/Http/Middleware/SetLocal.php
+++ app/Http/Middleware/SetLocal.php
@@ -15,22 +15,45 @@ class SetLocal
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+    // public function handle(Request $request, Closure $next): Response
+    // {
+    //     if ($request->hasHeader('local')) {
+    //         $locale = $request->header('Accept-Language');
+    //         if (! in_array($locale, ['en', 'nl'])) {
+    //             abort(400);
+    //         } else {
+    //             App::setLocale($locale);
+    //           //  Session::put(['local',$locale]);
+    //             error_log("11111");
+    //             error_log(app()->getLocale());
+    //             error_log("11111");
+    //         }
+    //     } elseif (Session::has('local')) {
+    //         app()->setLocale(Session::get('local'));
+    //     }
+    //     return $next($request);
+    // }
     public function handle(Request $request, Closure $next): Response
     {
+
+        $supportedLocales = ['en', 'nl'];
+        $defaultLocale = 'en';
+
+
         if ($request->hasHeader('Accept-Language')) {
             $locale = $request->header('Accept-Language');
-            if (! in_array($locale, ['en', 'nl'])) {
-                abort(400);
-            } else {
+            if (in_array($locale, $supportedLocales)) {
                 App::setLocale($locale);
-              //  Session::put(['local',$locale]);
-                error_log("11111");
-                error_log(app()->getLocale());
-                error_log("11111");
+                Session::put('locale', $locale);
+            } else {
+                App::setLocale($defaultLocale);
             }
-        } elseif (Session::has('local')) {
-            app()->setLocale(Session::get('local'));
+        } elseif (Session::has('locale')) {
+            App::setLocale(Session::get('locale'));
+        } else {
+            App::setLocale($defaultLocale);
         }
+
         return $next($request);
     }
 }
