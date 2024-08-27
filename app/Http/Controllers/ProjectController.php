d7684ed58e2af2a4388f56bc1b78d13525fef6a0
ailDaoud <daoudali259@gmail.com>
2024-08-27 10:59:50 +0300

    2.8

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Validator;
 
 class ProjectController extends Controller
 {
+
     public function store(Request $request)
     {
         if ($request->has('client_id')) {
@@ -180,95 +181,97 @@ class ProjectController extends Controller
                     'phone_number' => $validatedData['phone_number'],
                 ]);
 
-                foreach ($validatedData['projects'] as $projectData) {
-                    $project = $client->projects()->create([
-                        'en_title' => $projectData['en_title'],
-                        'nl_title' => $projectData['nl_title'],
-                        'en_description' => $projectData['en_description'],
-                        'nl_description' => $projectData['nl_description'],
-                        //   'begin_date' => $projectData['begin_date'],
-                        //   'end_date' => $projectData['end_date'],
-                        'en_result' => $projectData['en_result'],
-                        'nl_result' => $projectData['nl_result'],
-                    ]);
+                //    foreach ($validatedData['projects'] as $projectData) {
+                $project = $client->projects()->create([
+                    'client_id' => $client->id,
+                    'en_title' => $validatedData['en_title'],
+                    'nl_title' => $validatedData['nl_title'],
+                    'en_description' => $validatedData['en_description'],
+                    'nl_description' => $validatedData['nl_description'],
+                    //   'begin_date' => $validatedData['begin_date'],
+                    //   'end_date' => $validatedData['end_date'],
+                    'en_result' => $validatedData['en_result'],
+                    'nl_result' => $validatedData['nl_result'],
+                ]);
 
-                    if (!empty($projectData['service_categories'])) {
-                        foreach ($projectData['service_categories'] as $service_categories) {
+                if (!empty($validatedData['service_categories'])) {
+                    foreach ($validatedData['service_categories'] as $service_categories) {
 
-                            $project->service_categories()->create(
-                                [
-                                    'en_service_name' => $service_categories['en_service_name'],
-                                    'nl_service_name' => $service_categories['nl_service_name'],
+                        $project->service_categories()->create(
+                            [
+                                'en_service_name' => $service_categories['en_service_name'],
+                                'nl_service_name' => $service_categories['nl_service_name'],
 
-                                ]
-                            );
-                        }
-                    }
-
-                    if (!empty($projectData['achievements'])) {
-                        foreach ($projectData['achievements'] as $achievements) {
-
-                            $project->achievements()->create(
-                                [
-                                    'en_achievement_name' => $achievements['en_achievement_name'],
-                                    'nl_achievement_name' => $achievements['nl_achievement_name'],
-                                    //   'en_how_we_achieved_it' => $achievements['en_how_we_achieved_it'],
-                                    //   'nl_how_we_achieved_it' => $achievements['nl_how_we_achieved_it'],
-
-                                ]
-                            );
-                        }
-                    }
-
-                    if (!empty($projectData['challenges'])) {
-                        foreach ($projectData['challenges'] as $challenges) {
-
-                            $project->challenges()->create(
-                                [
-                                    'en_challenge_name' => $challenges['en_challenge_name'],
-                                    'nl_challenge_name' => $challenges['nl_challenge_name'],
-                                    'en_challenge_description' => $challenges['en_challenge_description'],
-                                    'nl_challenge_description' => $challenges['nl_challenge_description'],
-
-                                ]
-                            );
-                        }
-                    }
-                    if (!empty($projectData['project_live_links'])) {
-                        foreach ($projectData['project_live_links'] as $project_live_links) {
-                            $project->project_live_links()->create(
-                                [
-                                    'link' => $project_live_links['link'],
-
-                                ]
-                            );
-                        }
-                    }
-                    if (!empty($projectData['project_technologies'])) {
-                        foreach ($projectData['project_technologies'] as $project_technologies) {
-                            $project->project_technologies()->create(
-                                [
-                                    'tools' => $project_technologies['tools'],
-                                ]
-                            );
-                        }
-                    }
-                    if ($request->hasFile('image_path')) {
-                        foreach ($request->file('image_path') as $file) {
-                            $filename = time() . '_' . $file->getClientOriginalName();
-                            $filePath = $file->storeAs('project_images', $filename, 'public');
-                            $project->project_images()->create([
-                                'image_path' => $filePath
-                            ]);
-                        }
+                            ]
+                        );
                     }
                 }
+
+                if (!empty($validatedData['achievements'])) {
+                    foreach ($validatedData['achievements'] as $achievements) {
+
+                        $project->achievements()->create(
+                            [
+                                'en_achievement_name' => $achievements['en_achievement_name'],
+                                'nl_achievement_name' => $achievements['nl_achievement_name'],
+                                //   'en_how_we_achieved_it' => $achievements['en_how_we_achieved_it'],
+                                //   'nl_how_we_achieved_it' => $achievements['nl_how_we_achieved_it'],
+
+                            ]
+                        );
+                    }
+                }
+
+                if (!empty($validatedData['challenges'])) {
+                    foreach ($validatedData['challenges'] as $challenges) {
+
+                        $project->challenges()->create(
+                            [
+                                'en_challenge_name' => $challenges['en_challenge_name'],
+                                'nl_challenge_name' => $challenges['nl_challenge_name'],
+                                'en_challenge_description' => $challenges['en_challenge_description'],
+                                'nl_challenge_description' => $challenges['nl_challenge_description'],
+
+                            ]
+                        );
+                    }
+                }
+                if (!empty($validatedData['project_live_links'])) {
+                    foreach ($validatedData['project_live_links'] as $project_live_links) {
+                        $project->project_live_links()->create(
+                            [
+                                'link' => $project_live_links['link'],
+
+                            ]
+                        );
+                    }
+                }
+                if (!empty($validatedData['project_technologies'])) {
+                    foreach ($validatedData['project_technologies'] as $project_technologies) {
+                        $project->project_technologies()->create(
+                            [
+                                'tools' => $project_technologies['tools'],
+                            ]
+                        );
+                    }
+                }
+                if ($request->hasFile('image_path')) {
+                    foreach ($request->file('image_path') as $file) {
+                        $filename = time() . '_' . $file->getClientOriginalName();
+                        $filePath = $file->storeAs('project_images', $filename, 'public');
+                        $project->project_images()->create([
+                            'image_path' => $filePath
+                        ]);
+                    }
+                }
+
 
                 DB::commit();
                 return response()->json([
                     'sucsess' => 1,
                     'result' => $client,
                     'message' => "Client With His project Stored Sucsessfully",
+
                 ], 200);
 
                 //  return redirect()->route('clients.index')->with('success', 'Client created successfully.');
