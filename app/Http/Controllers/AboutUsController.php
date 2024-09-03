a402f85c54d8b5b1a7f39fb75bbb30da77ab77c0
ailDaoud <daoudali259@gmail.com>
2024-09-03 10:54:54 +0300

    4.1

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -55,6 +55,7 @@ class AboutUsController extends Controller
                     return [
                         'client_name' => $testimonial->client_name,
                         'client_testimonial' => $locale == 'en' ? $testimonial->en_client_testimonial : $testimonial->nl_client_testimonial,
+                        'image' => $testimonial->image_path
                     ];
                 }),
             ];
@@ -68,11 +69,10 @@ class AboutUsController extends Controller
                 'id' => $service->id,
                 'name' => $locale == 'en' ? $service->en_name : $service->nl_name,
                 'description' => $locale == 'en' ? $service->en_description : $service->nl_description,
-                'image' => $service->service_images->map(function ($related) use ($locale) {
+                'images' => $service->service_images->map(function ($related) use ($locale) {
 
-                    return [
-                        'image_path' => asset('storage/' . $related->image_path)
-                    ];
+
+                    return  asset('storage/' . $related->image_path);
                 }),
             ];
         });
@@ -85,11 +85,10 @@ class AboutUsController extends Controller
                 'id' => $project->id,
                 'title' => $locale == 'en' ? $project->en_title : $project->nl_title,
                 'description' => $locale == 'en' ? $project->en_description : $project->nl_description,
-                'image' => $project->project_images->map(function ($related) use ($locale) {
+                'images' => $project->project_images->map(function ($related) use ($locale) {
 
-                    return [
-                        'image_path' => asset('storage/' . $related->image_path)
-                    ];
+                    return
+                        asset('storage/' . $related->image_path);
                 }),
 
             ];
@@ -97,23 +96,14 @@ class AboutUsController extends Controller
 
 
         $contact = ContactsPage::first();
-        // $data['contact_page']['whats_next'] = $contacts->map(function ($contact) use ($locale) {
-        //return [
-        //   'id' => $contact->id,
-        $data['contact_page']['whats_next']['title'] = $locale == 'en' ? $contact->en_title : $contact->nl_title;
-        $data['contact_page']['whats_next']['sub_title'] = $locale == 'en' ? $contact->en_sub_title : $contact->nl_sub_title;
-        $data['contact_page']['whats_next']['whats_next_steps'] = $contact->contacts_whats_next->map(function ($subrelated) use ($locale, $contact) {
+        $data['whats_next']['title'] = $locale == 'en' ? $contact->en_title : $contact->nl_title;
+        $data['whats_next']['sub_title'] = $locale == 'en' ? $contact->en_sub_title : $contact->nl_sub_title;
+        $data['whats_next']['whats_next_steps'] = $contact->contacts_whats_next->map(function ($subrelated) use ($locale, $contact) {
             return [
                 'step_no' => $subrelated->step_no,
                 'step' => $locale == 'en' ? $subrelated->en_step : $subrelated->nl_step,
-
-                //     'title' => $locale == 'en' ? $contact->en_title : $contact->nl_title,
-                //    'sub_title' => $locale == 'en' ? $contact->en_sub_title : $contact->nl_sub_title,
             ];
         });
-
-        //    ];
-        //  });
 
         return response()->json([
             'success' => 1,
@@ -199,7 +189,7 @@ class AboutUsController extends Controller
 
 
 
-        return view('admin.home', compact('processed', 'members','processedServices','processedAboutus'));
+        return view('admin.home', compact('processed', 'members', 'processedServices', 'processedAboutus'));
     }
 
 
@@ -209,7 +199,7 @@ class AboutUsController extends Controller
         $language = $request->header('Accept-Language');
         $defaultLanguage = 'en';
         $locale = $language ? substr($language, 0, 2) : $defaultLanguage;
-        //   $data = [];
+        //$data = [];
         $aboutus = AboutUs::with(['steps_process', 'client_testimonial', 'for_who_services'])->get();
 
         if ($aboutus->isEmpty()) {
@@ -223,8 +213,19 @@ class AboutUsController extends Controller
         try {
 
             $processedAboutus = $aboutus->map(function ($about) use ($locale, $defaultLanguage) {
+                $heros = HeroSection::all();
+                $data['hero_section'] = $heros->map(function ($hero) use ($locale) {
 
-                $data = [
+                    return [
+                        'id' => $hero->id,
+                        'title' => $locale == 'en' ? $hero->en_title : $hero->nl_title,
+                        'sub_title' => $locale == 'en' ? $hero->en_sub_title : $hero->nl_sub_title,
+                        'image' => asset('storage/' . $hero->image_path)
+
+                    ];
+                });
+
+                $data['about_us'] = [
                     'id' => $about->id,
                     'company_name' => $locale == 'en' ? $about->en_company_name : $about->nl_company_name,
                     'introduction' => $locale == 'en' ? $about->en_introduction : $about->nl_introduction,
@@ -232,7 +233,7 @@ class AboutUsController extends Controller
                     'our_goals' => $locale == 'en' ? $about->en_our_goals : $about->nl_our_goals,
                     'title_for_who' => $locale == 'en' ? $about->en_title_for_who : $about->nl_title_for_who,
                     'title_steps_process' => $locale == 'en' ? $about->en_title_steps_process : $about->nl_title_steps_process,
-                    'meet_our_team' => $locale == 'en' ? $about->en_meet_our_team : $about->nl_meet_our_team,
+                    //   'meet_our_team' => $locale == 'en' ? $about->en_meet_our_team : $about->nl_meet_our_team,
                     'our_partners_associates' => $locale == 'en' ? $about->en_our_partners_associates : $about->nl_our_partners_associates,
                     'end' => $locale == 'en' ? $about->en_end : $about->nl_end,
                 ];
@@ -252,17 +253,49 @@ class AboutUsController extends Controller
                     return [
                         'client_name' => $testimonial->client_name,
                         'client_testimonial' => $locale == 'en' ? $testimonial->en_client_testimonial : $testimonial->nl_client_testimonial,
+                        'image' => $testimonial->image_path
                     ];
                 });
 
                 // Process For Who Services
-                $data['for_who_services'] = $about->for_who_services->map(function ($service) use ($locale) {
+                $data['for_who'] = $about->for_who_services->map(function ($service) use ($locale) {
                     return [
-                        'name_of_service_for_who' => $locale == 'en' ? $service->en_name : $service->nl_name,
-                        'description_of_service_for_who' => $locale == 'en' ? $service->en_description : $service->nl_description,
+                        'name' => $locale == 'en' ? $service->en_name : $service->nl_name,
+                        'description' => $locale == 'en' ? $service->en_description : $service->nl_description,
                     ];
                 });
+                ////
+                /*   $data['meet_our_team'] = $about->steps_process->map(function ($step) use ($locale,$about) {
+
+                    return [
+                        'title' => $locale == 'en' ? $about->en_title_meet_our_team : $step->nl_title_meet_our_team,
+                        'process_name' => $locale == 'en' ? $about->en_sub_title_meet_our_team : $step->nl_sub_title_meet_our_team,
+                        'team_members' =>$members->map(function ($member) use ($locale) {
+                            return [
+
+                                'name' => $member->name,
+                                'position' => $member->position,
+                                'description' => $member->descciption,
+                                'image_path' => asset('storage/' . $member->image_path)
+
+                            ];
+                        }),
+                    ];
+                });*/
                 $members = TeamMember::all();
+                $data['meet_our_team']['title'] = 'en' ? $about->en_title_meet_our_team : $about->nl_title_meet_our_team;
+                $data['meet_our_team']['sub_title'] = 'en' ? $about->en_title_meet_our_team : $about->nl_title_meet_our_team;
+                $data['meet_our_team']['team_members'] = $members->map(function ($member) use ($locale) {
+                    return [
+
+                        'name' => $member->name,
+                        'position' => $member->position,
+                        'description' => $member->descciption,
+                        'image_path' => asset('storage/' . $member->image_path)
+                    ];
+                });
+                //////
+                /*  $members = TeamMember::all();
                 $data['team_members'] = $members->map(function ($member) use ($locale) {
                     return [
 
@@ -272,49 +305,19 @@ class AboutUsController extends Controller
                         'image_path' => asset('storage/' . $member->image_path)
 
                     ];
-                });
+                });*/
 
 
-                $contacts = ContactsPage::all();
-                $data['contact_page']['whats_next'] = $contacts->map(function ($contact) use ($locale) {
+                $contact = ContactsPage::first();
+                $data['whats_next']['title'] = $locale == 'en' ? $contact->en_title : $contact->nl_title;
+                $data['whats_next']['sub_title'] = $locale == 'en' ? $contact->en_sub_title : $contact->nl_sub_title;
+                $data['whats_next']['whats_next_steps'] = $contact->contacts_whats_next->map(function ($subrelated) use ($locale, $contact) {
                     return [
-                        //   'id' => $contact->id,
-                        'title' => $locale == 'en' ? $contact->en_title : $contact->nl_title,
-                        'sub_title' => $locale == 'en' ? $contact->en_sub_title : $contact->nl_sub_title,
-                        'whats_next_steps' => $contact->contacts_whats_next->map(function ($subrelated) use ($locale, $contact) {
-                            return [
-                                'step_no' => $subrelated->step_no,
-                                'step' => $locale == 'en' ? $subrelated->en_step : $subrelated->nl_step,
-                            ];
-                        }),
-
+                        'step_no' => $subrelated->step_no,
+                        'step' => $locale == 'en' ? $subrelated->en_step : $subrelated->nl_step,
                     ];
                 });
-                $services = Service::with(['requirment', 'service_benefits', 'service_processs', 'client_testimonial', 'service_images'])->get();
-                $data['services'] = $services->map(function ($service) use ($locale) {
-                    return [
-                        'id' => $service->id,
-                        'name' => $locale == 'en' ? $service->en_name : $service->nl_name,
-                        'description' => $locale == 'en' ? $service->en_description : $service->nl_description,
-                        'image' => $service->service_images->map(function ($related) use ($locale) {
 
-                            return [
-                                'image_path' => asset('storage/' . $related->image_path)
-                            ];
-                        }),
-                    ];
-                });
-                $heros = HeroSection::all();
-                $data['hero_section'] = $heros->map(function ($hero) use ($locale) {
-
-                    return [
-                        'id' => $hero->id,
-                        'title' => $locale == 'en' ? $hero->en_title : $hero->nl_title,
-                        'sub_title' => $locale == 'en' ? $hero->en_sub_title : $hero->nl_sub_title,
-                        'image' => asset('storage/' . $hero->image_path)
-
-                    ];
-                });
 
 
 
@@ -409,8 +412,8 @@ class AboutUsController extends Controller
             // 'nl_title_for_who' => 'required',
             // 'en_title_steps_process' => 'required',
             // 'nl_title_steps_process' => 'required',
-            'en_meet_our_team' => 'required',
-            'nl_meet_our_team' => 'required',
+            //  'en_meet_our_team' => 'required',
+            //  'nl_meet_our_team' => 'required',
             'en_our_partners_associates' => 'required',
             'nl_our_partners_associates' => 'required',
             'en_end' => 'required',
@@ -445,8 +448,8 @@ class AboutUsController extends Controller
                 'nl_title_for_who' => $validatedData['nl_title_for_who'],
                 'en_title_steps_process' => $validatedData['en_title_steps_process'],
                 'nl_title_steps_process' => $validatedData['nl_title_steps_process'],
-                'en_meet_our_team' => $validatedData['en_meet_our_team'],
-                'nl_meet_our_team' => $validatedData['nl_meet_our_team'],
+                //  'en_meet_our_team' => $validatedData['en_meet_our_team'],
+                //  'nl_meet_our_team' => $validatedData['nl_meet_our_team'],
                 'en_our_partners_associates' => $validatedData['en_our_partners_associates'],
                 'nl_our_partners_associates' => $validatedData['nl_our_partners_associates'],
                 'en_end' => $validatedData['en_end'],
